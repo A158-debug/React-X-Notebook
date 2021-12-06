e00e563b36207356de411097c01cea495af5d9ad
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2021-12-06 15:33:21 +0530

    setup log in page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { NoteState } from './Context/NotesUser/NoteState'
 import { Navbar } from './components/Navbar';
 import { Home } from './components/Home';
-// import { Login } from './components/Login';
-// import { Signup } from './components/Signup';
+import { Login } from './components/Login';
+import { Signup } from './components/Signup';
 import { About } from './components/About';
+import { Alert } from './components/Alert';
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,13 +15,22 @@ import {
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="create and save your personal notes" />
+          <div className='container my-3'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+            </Routes>
+          </div>
+
+
+        </Router>
+      </NoteState>
     </>
   );
 }
